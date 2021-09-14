466cb580a64fd484b498b7d4e43edf05c83090b2
Kumaran R <kumar1987an@gmail.com>
2021-09-14 12:09:51 +0530

    .

--- Basics/2_Variables/code.py
+++ Basics/2_Variables/code.py
@@ -24,8 +24,29 @@ from keyword import kwlist
 for item in kwlist:
     print(item, end=", ")
 
-
+print("\n")
 print(name)
 print(NAME)
 print(employee_1)
 print(ELEMENT_465)
+
+college_passed_out_year = 2009  # Snake Case # Variables and Functions
+College_Passed_Out_Year = 2009  # Pascal Case # Classes
+College_passed_out_year = 2009  # Pascal Case # Classes
+college_Passed_Out_Year = 2009  # Camel Case
+college_Passed_out_year = 2009  # Camel Case
+
+_length = 5
+print(_length)
+
+__length__ = 5
+print(__length__)
+
+_length_ = 5
+print(_length_)
+
+x, y, z = 45, 55, 65
+print(x, y, z)
+
+x = y = z = 10
+print(x, y, z)
---
+++ Basics/3_Objects_in_python/code.py
@@ -0,0 +1,44 @@
+name = "Kumaran"
+dob = "12-05-2100"
+height = 150.7
+door_number = 4
+pincode = 600073
+favorite_food = ["icecreams", "chocolates", "cakes"]
+personal_data = {"home_address": "xyz Colony", "City": "Chennai"}
+set_data = {1, 2, 3, 4, 5, 6, 7}
+tuple_data = "78489hddffj", "&64u()ju"
+
+print(name)
+print(dob)
+print(height)
+print(door_number)
+print(pincode)
+print(favorite_food)
+print(personal_data)
+print(set_data)
+print(tuple_data)
+print("\n")
+print(type(name))
+print(type(dob))
+print(type(height))
+print(type(door_number))
+print(type(pincode))
+print(type(favorite_food))
+print(type(personal_data))
+print(type(set_data))
+print(type(tuple_data))
+
+print(name.lower)
+print(dob.isalnum)
+print(height.conjugate)
+print(door_number.__sizeof__)
+print(pincode.as_integer_ratio)
+print(favorite_food.append)
+print(personal_data.copy)
+print(set_data.difference)
+print(tuple_data.count)
+
+
+print(name.__class__.__base__)
+print(object)
+print(help(object))
