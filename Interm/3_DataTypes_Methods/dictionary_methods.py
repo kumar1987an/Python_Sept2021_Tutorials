52ba8358b3a34d604c4d16a36f8a25fd228cc93d
Kumaran R <kumar1987an@gmail.com>
2021-09-26 21:18:09 +0530

    .

---
+++ Interm/3_DataTypes_Methods/dictionary_methods.py
@@ -0,0 +1,17 @@
+hollywood_movies = {
+    "Marvel": "Avengers",
+    "Disney": "Dumbo",
+    "Universal Studios": "FF9",
+    "Fox Studios": "Avatar",
+    "Pixar Studios": "Cars",
+    "Warner Bros": "Inception",
+}
+print(
+    """
+These are the methods available for dict class under 
+hollywood_movies variable, also these hollywood_movies methods would be 
+the same as dir(dict) as hollywood_movies have dict data type"""
+)
+print("======================================================")
+print(dir(hollywood_movies))
+print()
