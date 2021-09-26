280e630fd64b9d77e80c0154eb8625fba0b0d08d
Kumaran R <kumar1987an@gmail.com>
2021-09-26 21:27:22 +0530

    .

---
+++ Interm/3_DataTypes_Methods/set_methods.py
@@ -0,0 +1,14 @@
+a = {1, 2, 3, 4, 5}
+b = {2, 3, 4, 5, 6}
+c = {3, 4, 5, 6, 7}
+d = {4, 5, 6, 7, 8}
+
+print(
+    """
+These are the methods available for set class under 
+a, b, c, d variables, also these set (a,b,c,d) methods would be 
+the same as dir(set) as above variables have dict data type"""
+)
+print("======================================================")
+print(dir(a))
+print()
