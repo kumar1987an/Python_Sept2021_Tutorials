46b3fe19733dcc0667574f5c03370502cb1aca84
kumar1987an <kumar1987an@gmail.com>
2021-10-02 11:25:09 -0700

    .

--- Interm/7_lambda_functions/code.py
+++ Interm/7_lambda_functions/code.py
@@ -7,8 +7,3 @@ print((lambda x, y, z=3: x + y + z)(8, y=9))
 print((lambda *args: sum(args))(1, 2, 3))
 print((lambda **kwargs: sum(kwargs.values()))(one=1, two=2, three=3))
 print((lambda x, y=0, z=0: x + y + z)(1, y=2, z=3))
-
-
-d1 = dict(a=10, b=20)
-d2 = dict(c=10, b=10)
-print({**d1, **d2})
