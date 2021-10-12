0f4fa28e22685ecb0998d33f45631c3e2dc6a7db
Kumaran R <kumar1987an@gmail.com>
2021-10-12 13:00:33 +0530

    .

--- Advanced/1_decorators/code.py
+++ Advanced/1_decorators/code.py
@@ -36,13 +36,13 @@
 # Decorator 1
 
 
-def my_decorator(func):
-    def wrapper():
-        print("Something is happeneing before the function is called")
-        func()
-        print("Something is happeneing after the function is called")
+# def my_decorator(func):
+#     def wrapper():
+#         print("Something is happeneing before the function is called")
+#         func()  # it is say_hello
+#         print("Something is happeneing after the function is called")
 
-    return wrapper
+#     return wrapper
 
 
 # @my_decorator
@@ -55,6 +55,7 @@ def my_decorator(func):
 
 
 # Decorator 2
+
 
 # def do_twice(func):
 #     def wrapper_do_twice():
@@ -73,25 +74,79 @@ def my_decorator(func):
 
 # Decorator 3
 
-
-def do_twice(func):
-    def wrapper_do_twice(*args, **kwargs):
-        func(*args, **kwargs)
-        return func(*args, **kwargs)
-
-    return wrapper_do_twice
+from functools import wraps
 
 
-@do_twice
+# def do_twice(func):
+#     @wraps(func)
+#     def wrapper_do_twice(*args, **kwargs):
+#         func(*args, **kwargs)
+#         return func(*args, **kwargs)
+
+#     return wrapper_do_twice
+
+
+# @do_twice
+# def greet(name):
+#     print("Creating Greeting")
+#     print(f"Hi {name}")
+
+
+# greet("Kumaran")
+
+
+# Boilerplate template (readymade template)
+
+
+# def decorated_function(func):
+#     @wraps(func)
+#     def wrapped_function(*args, **kwargs):
+#         value = func(*args, **kwargs)
+#         return value
+
+#     return wrapped_function
+
+# REal world examples
+
+# import time
+
+
+# def timer(func):
+#     """Print the runtime of the decorated function"""
+
+#     @wraps(func)
+#     def wrapper_timer(*args, **kwargs):
+#         start_time = time.perf_counter()
+#         value = func(*args, **kwargs)
+#         end_time = time.perf_counter()
+#         run_time = end_time - start_time
+#         print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+#         return value
+
+#     return wrapper_timer
+
+
+# @timer
+# def waste_some_time(num_times):
+#     for _ in range(num_times):
+#         sum([i ** 2 for i in range(10000)])
+#     else:
+#         return "Loop finished"
+
+
+# print(waste_some_time(1000))
+# print(waste_some_time.__name__)
+# line 117 ==> wrapping  wrapper_timer to waste_some_time
+# =====================================================================
+
+
+from repeater import repeat
+
+
+@repeat(num_times=4)
 def greet(name):
-    print("Creating Greeting")
-    print(f"Hi {name}")
+    print(f"Hello {name}")
 
 
-greet("ZZZ")
-
-
-# Creating Greeting
-# Hi ZZZ
-# Creating Greeting
-# Hi ZZZ
+greet("Friends")
+print(greet.__name__)
