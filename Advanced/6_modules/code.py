af99151d207eaa6f5d95d1e955a18be43cfd461f
Kumaran R <37325822+kumar1987an@users.noreply.github.com>
2021-10-14 17:28:24 +0530

    .

---
+++ Advanced/6_modules/code.py
@@ -0,0 +1,42 @@
+# dataclasses Module
+
+
+from dataclasses import dataclass
+
+
+@dataclass
+class DataClassCard:
+    rank: str
+    suit: str
+
+
+queen_of_hearts = DataClassCard("Q", "Hearts")
+print(queen_of_hearts)
+print(queen_of_hearts.rank)
+print(queen_of_hearts.suit)
+print(queen_of_hearts == DataClassCard("Q", "Hearts"))
+
+
+class NormalDataClassCard:
+    def __init__(self, rank, suit) -> None:
+        self.rank = rank
+        self.suit = suit
+
+
+normal_queen_of_hearts = NormalDataClassCard("Q", "Hearts")
+print(normal_queen_of_hearts)
+print(normal_queen_of_hearts == NormalDataClassCard("Q", "Hearts"))
+
+
+# Collections Module
+
+from collections import namedtuple
+
+Person = namedtuple("Person", ["age", "heigt", "name"])
+dave = Person(20, 178, "Dave")
+print(dave)
+jack = Person(age=30, heigt=170, name="Jack S")
+print(jack)
+print(jack.name)
+print(jack.heigt)
+print(jack.age)
